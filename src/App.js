42f9c61e38eddb144511c2926937de60553923f0
MK-Aravindan <aravindanmk02@gmail.com>
2023-02-28 09:33:39 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const API_URL = "https://official-joke-api.appspot.com/random_joke";
 
 function App() {
+
+  const [items, setItems] = useState('')
+  const [punchline, setPunchline] = useState('')
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setItems(data))
+      .then(data => setPunchline(data.punchline))
+
+    setItems(false)
+    setPunchline(false)
+
+  }
+
+  useEffect(() => {
+    generateJoke()
+  }, [])
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setPunchline(items.punchline);
+    }, 3500);
+
+    return () => clearTimeout(timeoutId);
+  }, [items]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Wanna hear some Jokes</h2>
+        <p>{items.setup}</p>
+        <p>{punchline}</p>
+      <button onClick={generateJoke}>Get new Joke</button>
     </div>
   );
+
 }
 
 export default App;
